0f8b6179826fc68cdfc20b807c4c9840ae7e55a1
yo-ch <yoson.chiu@hotmail.com>
2017-07-11 20:48:38 -0400

    Style updates.

--- tab.js
+++ tab.js
@@ -1,14 +1,16 @@
 updateTime();
+setInterval(updateTime, 1000);
 
 function updateTime() {
     document.getElementById("time").innerHTML = getTime();
-    setTimeout(updateTime, 1000);
 }
 
 function getTime() {
     var d = new Date();
     var hours = d.getHours() > 12 ? d.getHours() - 12 : d.getHours();
+    hours = hours == 0 ? 12 : hours;
     hours = hours < 10 ? '0' + hours : hours;
+
     var minutes = d.getMinutes();
     minutes = minutes < 10 ? '0' + minutes : minutes;
     var seconds = d.getSeconds() < 10 ? '0' + d.getSeconds() : d.getSeconds();
