fb57f34f59e594575636372fe18e87d0daca52f4
yo-ch <yoson.chiu@hotmail.com>
2017-07-12 01:04:16 -0400

    Drag and drop background change added.
    Still need to store the new background for future new tabs though.

--- tab.js
+++ tab.js
@@ -1,6 +1,16 @@
+/*
+Main.
+*/
 updateTime();
 setInterval(updateTime, 1000);
 
+if (window.File && window.FileList && window.FileReader) {
+    fileDrag();
+}
+
+/*
+Functions.
+*/
 function updateTime() {
     document.getElementById("time").innerHTML = getTime();
 }
@@ -16,4 +26,48 @@ function getTime() {
     var seconds = d.getSeconds() < 10 ? '0' + d.getSeconds() : d.getSeconds();
 
     return `${hours} ${minutes} ${seconds}`;
+}
+
+function fileDrag() {
+    var filedrag = document.getElementById("filedrag");
+
+    filedrag.addEventListener("dragover", fileDragHover, false);
+    filedrag.addEventListener("dragleave", fileDragHover, false);
+    filedrag.addEventListener("drop", fileSelectHandler, false);
+}
+
+function fileDragHover(e) {
+    e.stopPropagation();
+    e.preventDefault();
+    e.target.className = (e.type == "dragover" ? "hover" : "");
+}
+
+function fileSelectHandler(e) {
+    //Cancel hover styling.
+    fileDragHover(e);
+
+    //Select image and change wallpaper.
+    var file = e.dataTransfer.files[0];
+    var imagePatt = /^image\//;
+    if (!imagePatt.test(file.type)) return;
+
+    var reader = new FileReader();
+    reader.onload = function(e) {
+        changeBackground(e.target.result);
+    }
+    reader.readAsDataURL(file);
+}
+
+/*
+Crossfade old and new background.
+*/
+function changeBackground(source) {
+    var image = document.getElementById("backimage");
+    var oldImage = document.getElementById("backimageold");
+    oldImage.src = image.src;
+    oldImage.className = "hover";
+    image.src = `${source}`;
+    setTimeout(function() { //Delete old background after transition.
+        oldImage.src = "";
+    }, 2000);
 }
